55674b777f5ae9c8073d44ef6cc60c3833333ca2
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-11-06 22:26:15 +0900

    test(store-output): 상품 목록 정보 출력 기능 테스트 추가

---
+++ src/test/java/store/model/StockTest.java
@@ -0,0 +1,57 @@
+package store.model;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class StockTest {
+
+    @Test
+    @DisplayName("재고 출력")
+    void print_stock() {
+        assertSimpleTest(() -> {
+            Stock stock = new Stock(List.of(
+                    Product.from("콜라,1000,10,탄산2+1"),
+                    Product.from("콜라,1000,10,null"),
+                    Product.from("사이다,1000,8,탄산2+1"),
+                    Product.from("사이다,1000,7,null"),
+                    Product.from("오렌지주스,1800,9,MD추천상품"),
+                    Product.from("오렌지주스,1800,0,null"),
+                    Product.from("탄산수,1200,5,탄산2+1"),
+                    Product.from("탄산수,1200,0,null"),
+                    Product.from("물,500,10,null"),
+                    Product.from("비타민워터,1500,6,null"),
+                    Product.from("감자칩,1500,5,반짝할인"),
+                    Product.from("감자칩,1500,5,null"),
+                    Product.from("초코바,1200,5,MD추천상품"),
+                    Product.from("초코바,1200,5,null"),
+                    Product.from("에너지바,2000,5,null"),
+                    Product.from("정식도시락,6400,8,null"),
+                    Product.from("컵라면,1700,1,MD추천상품"),
+                    Product.from("컵라면,1700,10,null")
+            ));
+            assertThat(stock.toString()).contains(
+                    "- 콜라 1,000원 10개 탄산2+1",
+                    "- 콜라 1,000원 10개",
+                    "- 사이다 1,000원 8개 탄산2+1",
+                    "- 사이다 1,000원 7개",
+                    "- 오렌지주스 1,800원 9개 MD추천상품",
+                    "- 오렌지주스 1,800원 재고 없음",
+                    "- 탄산수 1,200원 5개 탄산2+1",
+                    "- 탄산수 1,200원 재고 없음",
+                    "- 물 500원 10개",
+                    "- 비타민워터 1,500원 6개",
+                    "- 감자칩 1,500원 5개 반짝할인",
+                    "- 감자칩 1,500원 5개",
+                    "- 초코바 1,200원 5개 MD추천상품",
+                    "- 초코바 1,200원 5개",
+                    "- 에너지바 2,000원 5개",
+                    "- 정식도시락 6,400원 8개",
+                    "- 컵라면 1,700원 1개 MD추천상품",
+                    "- 컵라면 1,700원 10개");
+        });
+    }
+}
