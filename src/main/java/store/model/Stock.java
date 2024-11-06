dda54503c8e2229dd692759701adc630d43245e8
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-11-06 22:26:04 +0900

    feat(store-output): 상품 목록 정보 출력 기능 추가

---
+++ src/main/java/store/model/Stock.java
@@ -0,0 +1,19 @@
+package store.model;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Stock {
+    private List<Product> products;
+
+    public Stock(List<Product> products) {
+        this.products = products;
+    }
+
+    @Override
+    public String toString() {
+        return products.stream()
+                .map(Product::toString)
+                .collect(Collectors.joining("\n"));
+    }
+}
