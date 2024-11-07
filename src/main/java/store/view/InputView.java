526ad287d35dada071500236655a8e4fce51e34b
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-11-08 04:53:59 +0900

    feat(store-exception): 존재하지 않는 상품명 예외 기능 추가

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -4,19 +4,38 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
 import store.model.OrderItem;
+import store.model.Product;
+import store.model.Stock;
+import store.repositroy.StoreRepository;
 
 public class InputView {
+    private final Stock stock;
 
-    public List<OrderItem> readItem() {
+    public InputView() {
+        this.stock = new Stock(new StoreRepository().getProducts());
+    }
+
+    public void readItem() {
         while (true) {
             System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
             try {
                 String input = Console.readLine();
-                return createOrder(input);
+                List<OrderItem> order = createOrder(input);
+                for (OrderItem orderItem : order) {
+                    isExistProduct(orderItem.name());
+                }
             } catch (Exception e) {
-                System.out.println("[ERROR] " + e.getMessage());
+                System.out.println("[ERROR] " + e.getMessage() + "다시 입력 해주세요.");
             }
         }
+    }
+
+    public List<Product> isExistProduct(String productName) {
+        List<Product> product = stock.getProductByName(productName);
+        if (product.isEmpty()) {
+            throw new IllegalArgumentException("존재하지 않는 상품입니다.");
+        }
+        return product;
     }
 
     private List<OrderItem> createOrder(String orders) {
