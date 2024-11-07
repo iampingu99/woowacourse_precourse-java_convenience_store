8e045e3c0a701519fdde9b0b40e9ccd68dae7727
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-11-08 02:46:25 +0900

    test(store-exception): 구매 상품과 수량 형식 예외 기능 테스트 추가

--- src/test/java/store/model/OrderItemTest.java
+++ src/test/java/store/model/OrderItemTest.java
@@ -2,6 +2,7 @@ package store.model;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,5 +17,14 @@ class OrderItemTest {
             assertThat(item.name()).isEqualTo(new OrderItem("사이다", 2).name());
             assertThat(item.quantity()).isEqualTo(new OrderItem("사이다", 2).quantity());
         });
+    }
+
+    @Test
+    @DisplayName("구매할 상품의 수량이 0이거나 숫자가 아닌 경우 예외가 발생한다")
+    void product_quantity_not_number() {
+        assertThatThrownBy(() -> OrderItem.from("[사이다-a]"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> OrderItem.from("[사이다-0]"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
