400de35b40d909e16b88c08d7e78036435ffcf1a
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-11-09 19:39:54 +0900

    refactor(store-logic): 구매 동작 기능 과다로 인한 메서드 추출

--- src/main/java/lab/service/StoreService.java
+++ src/main/java/lab/service/StoreService.java
@@ -38,27 +38,35 @@ public class StoreService {
             if (promotionStock == null) { //일반 재고만 존재하는 경우
                 product.addQuantity(ProductType.REGULAR, item.quantity());
             } else if (promotionStock > item.quantity()) {//프로모션 재고로 구매 가능한 경우
-                int promotionUnit = promotion.getBuy() + promotion.getGet();
-                if (item.quantity() % promotionUnit == promotion.getBuy()) {
-                    String confirm = confirm("현재 " + item.name() + "은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)");
-                    if (confirm.equals("Y")) {
-                        product.purchase(ProductType.PROMOTION, item.quantity() + 1);
-                    } else {
-                        product.purchase(ProductType.PROMOTION, item.quantity());
-                    }
-                }
-                product.addQuantity(ProductType.PROMOTION, promotionStock);
+                promotionPurchase(item, promotion, product, promotionStock);
             } else { //일반 재고 + 프로모션 재고로 구매 가능한 경우
-                int changeStock = calculateNonPromotionQuantity(promotion, promotionStock, item.quantity());
-                String confirm = confirm(
-                        "현재 " + item.name() + " " + changeStock + "개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)");
-                if (confirm.equals("Y")) {
-                    product.purchase(ProductType.PROMOTION, promotionStock);
-                    product.purchase(ProductType.REGULAR, item.quantity() - promotionStock);
-                } else {
-                    product.purchase(ProductType.PROMOTION, promotionStock);
-                }
+                mixPurchase(item, promotion, promotionStock, product);
             }
+        }
+    }
+
+    private void promotionPurchase(OrderItem item, Promotion promotion, Product product, Integer promotionStock) {
+        int promotionUnit = promotion.getBuy() + promotion.getGet();
+        if (item.quantity() % promotionUnit == promotion.getBuy()) {
+            String confirm = confirm("현재 " + item.name() + "은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)");
+            if (confirm.equals("Y")) {
+                product.purchase(ProductType.PROMOTION, item.quantity() + 1);
+            } else {
+                product.purchase(ProductType.PROMOTION, item.quantity());
+            }
+        }
+        product.addQuantity(ProductType.PROMOTION, promotionStock);
+    }
+
+    private void mixPurchase(OrderItem item, Promotion promotion, Integer promotionStock, Product product) {
+        int changeStock = calculateNonPromotionQuantity(promotion, promotionStock, item.quantity());
+        String confirm = confirm(
+                "현재 " + item.name() + " " + changeStock + "개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)");
+        if (confirm.equals("Y")) {
+            product.purchase(ProductType.PROMOTION, promotionStock);
+            product.purchase(ProductType.REGULAR, item.quantity() - promotionStock);
+        } else {
+            product.purchase(ProductType.PROMOTION, promotionStock);
         }
     }
 
