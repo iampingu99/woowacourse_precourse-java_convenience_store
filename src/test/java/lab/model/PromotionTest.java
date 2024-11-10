5ee5e256045760982236113af7fe8228b675aeba
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-11-11 00:34:03 +0900

    feat(store-logic): 프로모션 적용 기간 확인 기능 테스트 추가

---
+++ src/test/java/lab/model/PromotionTest.java
@@ -0,0 +1,27 @@
+package lab.model;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertNowTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import camp.nextstep.edu.missionutils.DateTimes;
+import java.time.LocalDate;
+import org.junit.jupiter.api.Test;
+
+class PromotionTest {
+
+    @Test
+    void 프로모션_적용_기간_참() {
+        assertNowTest(() -> {
+            Promotion promotion = Promotion.from("탄산2+1,2,1,2024-01-01,2024-12-31");
+            assertThat(promotion.isAvailable(DateTimes.now())).isEqualTo(true);
+        }, LocalDate.of(2024, 2, 1).atStartOfDay());
+    }
+
+    @Test
+    void 프로모션_적용_기간_거짓() {
+        assertNowTest(() -> {
+            Promotion promotion = Promotion.from("반짝할인,1,1,2024-11-01,2024-11-30");
+            assertThat(promotion.isAvailable(DateTimes.now())).isEqualTo(false);
+        }, LocalDate.of(2024, 2, 1).atStartOfDay());
+    }
+}
