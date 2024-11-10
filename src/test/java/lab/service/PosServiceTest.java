395ce135efc554462dcbf596a7c7148743231114
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-11-11 00:52:51 +0900

    test(store-logic): 포스기에 증정 상품 등록 기능 테스트 추가

--- src/test/java/lab/service/PosServiceTest.java
+++ src/test/java/lab/service/PosServiceTest.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 import lab.model.Product;
 import lab.model.ProductType;
+import lab.model.Promotion;
 import lab.model.Receipts;
 import org.junit.jupiter.api.Test;
 
@@ -25,6 +26,24 @@ class PosServiceTest {
             posService.addPurchaseProduct(purchaseQuantities, product);
 
             assertThat(receipts.getPurchaseProducts().get(0).getQuantities()).isEqualTo(purchaseQuantities);
+        });
+    }
+
+    @Test
+    void 포스기에_증정_물건_등록() {
+        assertSimpleTest(() -> {
+            Map<ProductType, Integer> purchaseQuantities = new HashMap<>();
+            purchaseQuantities.put(ProductType.PROMOTION, 10);
+            purchaseQuantities.put(ProductType.REGULAR, 5);
+            Product product = new Product("콜라", 1000);
+            Promotion promotion = Promotion.from("탄산2+1,2,1,2024-01-01,2024-12-31");
+            product.setPromotion(promotion);
+
+            Receipts receipts = new Receipts();
+            PosService posService = new PosService(receipts);
+            posService.addFreeProduct(purchaseQuantities, product);
+
+            assertThat(receipts.getFreeProducts().get(0).getQuantities().get(ProductType.PROMOTION)).isEqualTo(3);
         });
     }
 }
