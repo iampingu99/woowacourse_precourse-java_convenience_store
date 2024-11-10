76f1e87b5c5cf45862fa55f5f51719f78dc8f13a
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-11-11 04:03:59 +0900

    refactor(store-view): 입출력 정적 메서드로 수정

--- src/main/java/lab/view/OutputView.java
+++ src/main/java/lab/view/OutputView.java
@@ -4,18 +4,19 @@ package lab.view;
 import lab.model.Stock;
 
 public class OutputView {
-    private final String WELCOME_MESSAGE = "안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.";
-    private final String ERROR = "[ERROR] ";
+    private static final String WELCOME_MESSAGE = "안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.";
+    private static final String ERROR = "[ERROR] ";
+    private static final String TRY_AGAIN = " 다시 입력해 주세요.";
 
-    public void printWelcomeMessage() {
+    public static void printWelcomeMessage() {
         System.out.println(WELCOME_MESSAGE);
     }
 
-    public void printStock(Stock stock) {
+    public static void printStock(Stock stock) {
         System.out.println(stock);
     }
 
-    public void printError(Exception exception) {
-        System.out.println(ERROR + exception.getMessage());
+    public static void printError(Exception exception) {
+        System.out.println(ERROR + exception.getMessage() + TRY_AGAIN);
     }
 }
