208e24b05d9a13dc876ae3c7c7fbd3dd29090fc2
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-11-11 03:56:16 +0900

    refactor(store-logic): 상품 구매 동작 분리
    - 구매 상품 수량 계산
    - 영수증 생성

--- src/main/java/lab/controller/StoreController.java
+++ src/main/java/lab/controller/StoreController.java
@@ -1,27 +1,39 @@
 package lab.controller;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import lab.dto.ReceiptDto;
 import lab.model.OrderItem;
-import lab.service.StoreService;
-import lab.view.InputView;
-import lab.view.OutputView;
+import lab.model.Product;
+import lab.model.ProductType;
+import lab.service.StockService;
 
 public class StoreController {
-    private final InputController inputController;
-    private final InputView inputView;
-    private final OutputView outputView;
+    private final PosController posController;
+    private final QuantityController quantityController;
+    private final StockService stockService;
 
-    public StoreController(InputController inputController, InputView inputView, OutputView outputView) {
-        this.inputController = inputController;
-        this.inputView = inputView;
-        this.outputView = outputView;
+    public StoreController(PosController posController, QuantityController quantityController,
+                           StockService stockService) {
+        this.posController = posController;
+        this.quantityController = quantityController;
+        this.stockService = stockService;
     }
 
-    public void run() {
-        StoreService storeService = new StoreService();
-        outputView.printWelcomeMessage();
-        outputView.printStock(storeService.getStock());
-        List<OrderItem> purchaseProduct = inputController.getPurchaseProduct(inputView, outputView);
-        System.out.println(purchaseProduct);
+    public void run(List<OrderItem> orderItems) {
+        for (OrderItem orderItem : orderItems) {
+            Product product = stockService.getProductOrThrow(orderItem.name());
+            stockService.validateQuantity(product, orderItem.quantity());
+            int promotionStock = product.getQuantityByKey(ProductType.PROMOTION);
+            Map<ProductType, Integer> purchaseQuantities
+                    = quantityController.getPurchasePromotionQuantity(product, orderItem, promotionStock);
+            for (Entry<ProductType, Integer> quantities : purchaseQuantities.entrySet()) {
+                product.purchase(quantities.getKey(), quantities.getValue());
+            }
+            posController.productScan(purchaseQuantities, product);
+        }
+        ReceiptDto receiptDto = posController.issueReceipt();
+        System.out.println(receiptDto.getView());
     }
 }
