a9193d35e91fb7108706faea6a142cfb66fafdbd
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-11-11 00:41:11 +0900

    test(store-logic): 프로모션 증정 상품 계산 기능 테스트 추가

--- src/test/java/lab/model/PromotionTest.java
+++ src/test/java/lab/model/PromotionTest.java
@@ -24,4 +24,14 @@ class PromotionTest {
             assertThat(promotion.isAvailable(DateTimes.now())).isEqualTo(false);
         }, LocalDate.of(2024, 2, 1).atStartOfDay());
     }
+
+    @Test
+    void 프로모션_증정_상품_개수_계산() {
+        assertNowTest(() -> {
+            Promotion promotion = Promotion.from("탄산2+1,2,1,2024-01-01,2024-12-31");
+            assertThat(promotion.calcFreeProductCount(3)).isEqualTo(1);
+            assertThat(promotion.calcFreeProductCount(5)).isEqualTo(1);
+            assertThat(promotion.calcFreeProductCount(10)).isEqualTo(3);
+        }, LocalDate.of(2024, 2, 1).atStartOfDay());
+    }
 }
