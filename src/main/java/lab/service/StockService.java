8a345cfb77c90275a2849f58487dbf862d1cf534
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-11-10 19:39:23 +0900

    feat(store-logic): 상품 재고 검색 기능 추가

--- src/main/java/lab/service/StockService.java
+++ src/main/java/lab/service/StockService.java
@@ -1,6 +1,8 @@
 package lab.service;
 
+import java.util.Map;
 import lab.model.Product;
+import lab.model.ProductType;
 import lab.model.Stock;
 
 public class StockService {
@@ -13,5 +15,15 @@ public class StockService {
     public Product getProductOrThrow(String productName) {
         return stock.findByProductName(productName)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 상품입니다."));
+    }
+
+    public Map<ProductType, Integer> getQuantitiesOrThrow(Product product, int purchaseQuantity) {
+        int totalCount = product.getQuantities().values().stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+        if (totalCount < purchaseQuantity) {
+            throw new IllegalArgumentException("재고 수량을 초과하여 구매할 수 없습니다.");
+        }
+        return product.getQuantities();
     }
 }
