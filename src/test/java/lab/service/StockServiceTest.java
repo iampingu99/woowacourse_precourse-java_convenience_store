b2829447c54c8845671be7c6538543c7a2ac5848
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-11-10 19:17:43 +0900

    test(store-logic): 상품 이름 검색 기능 테스트 추가

---
+++ src/test/java/lab/service/StockServiceTest.java
@@ -0,0 +1,56 @@
+package lab.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.Assertions;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+import lab.model.Product;
+import lab.model.ProductType;
+import lab.model.Promotion;
+import lab.model.Stock;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class StockServiceTest {
+
+    private StockService stockService;
+
+    @BeforeEach
+    void setUp() {
+        Map<String, Product> products = new HashMap<>();
+        Promotion promotion = new Promotion("탄산2+1", 2, 1, LocalDate.parse("2024-01-01"),
+                LocalDate.parse("2024-12-31"));
+        Product product = new Product("콜라", 1000);
+        product.addQuantity(ProductType.REGULAR, 10);
+        product.addQuantity(ProductType.PROMOTION, 10);
+        product.setPromotion(promotion);
+        products.put(product.getName(), product);
+
+        stockService = new StockService(new Stock(products));
+    }
+
+    @Test
+    void 상품_이름_검색() {
+        Assertions.assertSimpleTest(() -> {
+            Product findProduct = stockService.getProductOrThrow("콜라");
+
+            assertThat(findProduct.getName()).isEqualTo("콜라");
+            assertThat(findProduct.getPrice()).isEqualTo(1000);
+            assertThat(findProduct.getQuantities().get(ProductType.REGULAR)).isEqualTo(10);
+            assertThat(findProduct.getQuantities().get(ProductType.PROMOTION)).isEqualTo(10);
+
+            assertThat(findProduct.getPromotion().getName()).isEqualTo("탄산2+1");
+            assertThat(findProduct.getPromotion().getBuy()).isEqualTo(2);
+            assertThat(findProduct.getPromotion().getGet()).isEqualTo(1);
+        });
+    }
+
+    @Test
+    void 존재하지_않는_상품_이름인_경우_예외가_발생한다() {
+        assertThatThrownBy(() -> stockService.getProductOrThrow("사이다"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
