6956c56ffbaa570c808d38dd87d756fd341a511c
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-11-11 00:52:38 +0900

    feat(store-logic): 포스기에 증정 상품 등록 기능 추가

--- src/main/java/lab/service/PosService.java
+++ src/main/java/lab/service/PosService.java
@@ -1,8 +1,10 @@
 package lab.service;
 
+import java.util.HashMap;
 import java.util.Map;
 import lab.model.Product;
 import lab.model.ProductType;
+import lab.model.Promotion;
 import lab.model.Receipts;
 
 public class PosService {
@@ -16,5 +18,19 @@ public class PosService {
     public void addPurchaseProduct(Map<ProductType, Integer> purchaseQuantity, Product product) {
         Product purchaseProduct = Product.of(product, purchaseQuantity);
         receipts.addPurchaseProducts(purchaseProduct);
+    }
+
+    public int getFreeProductCount(Map<ProductType, Integer> purchaseQuantity, Product product) {
+        Promotion promotion = product.getPromotion();
+        int purchasePromotionQuantity = purchaseQuantity.get(ProductType.PROMOTION);
+        return promotion.calcFreeProductCount(purchasePromotionQuantity);
+    }
+
+    public void addFreeProduct(Map<ProductType, Integer> purchaseQuantity, Product product) {
+        int freeProductCount = getFreeProductCount(purchaseQuantity, product);
+        Map<ProductType, Integer> freeQuantities = new HashMap<>();
+        freeQuantities.put(ProductType.PROMOTION, freeProductCount);
+        Product freeProduct = Product.of(product, freeQuantities);
+        receipts.addFreeProducts(freeProduct);
     }
 }
