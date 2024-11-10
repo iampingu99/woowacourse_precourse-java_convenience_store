ec6aff245dd6e503300b5549976109f608370bbc
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2024-11-11 04:10:20 +0900

    feat(store-logic): 애플리케이션 실행 기능 추가

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,11 +1,51 @@
 package store;
 
-import store.view.InputView;
+import java.util.List;
+import lab.controller.InputController;
+import lab.controller.PosController;
+import lab.controller.QuantityController;
+import lab.controller.StoreController;
+import lab.model.OrderItem;
+import lab.model.Receipts;
+import lab.model.Stock;
+import lab.repository.ProductRepository;
+import lab.repository.PromotionRepository;
+import lab.service.PosService;
+import lab.service.StockFactory;
+import lab.service.StockService;
+import lab.utils.FileReader;
+import lab.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        InputView inputView = new InputView();
-        inputView.readItem();
+        FileReader fileReader = new FileReader();
+        ProductRepository productRepository = new ProductRepository(fileReader);
+        PromotionRepository promotionRepository = new PromotionRepository(fileReader);
+
+        StockFactory stockFactory = new StockFactory(productRepository, promotionRepository);
+        Stock stock = stockFactory.createStock();
+
+        PosController posController = new PosController(new PosService(new Receipts()));
+        QuantityController quantityController = new QuantityController();
+        StoreController storeController = new StoreController(posController, quantityController,
+                new StockService(stock));
+
+        InputController inputController = new InputController();
+
+        while (true) {
+            try {
+                OutputView.printWelcomeMessage();
+                OutputView.printStock(stock);
+                List<OrderItem> purchaseProduct = inputController.getPurchaseProduct();
+                storeController.run(purchaseProduct);
+                String input = inputController.confirmAdditionalPurchase();
+                if (input.equals("N")) {
+                    return;
+                }
+            } catch (IllegalArgumentException e) {
+                OutputView.printError(e);
+            }
+        }
     }
 }
